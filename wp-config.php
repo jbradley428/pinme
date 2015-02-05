eb056fcf66c71bd2f341c0158f82f0462eb363a7
Jamie Bradley <jbradley428@gmail.com>
2015-02-05 18:23:26 -0500

    - changed background & color scheme (also thinking of going to coral/gold too) - added sublime project & workplaces

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,14 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'pinteres_PinLove');
+//define('DB_NAME', 'pinteres_PinLove');
+define('DB_NAME', 'pinme');
 
 /** MySQL database username */
-define('DB_USER', 'pinteres_PinLove');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '532@SPcf1-');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
